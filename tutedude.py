76589de2533fd6b283fac826e40df1188411f9f0
janhavisathawane-creator <janhavisathawane@gmail.com>
2025-12-10 20:24:43 +0530

    Add files via upload

---
+++ tutedude.py
@@ -0,0 +1,6 @@
+num1= int(input("enter  first number "))
+num2=int(input("enter second number"))
+print("addition :" ,  num1 + num2 )
+print("subtraction :" , num1 - num2)
+print("multiplication :" , num1 * num2)
+print("divison :" , num1 / num2)
